240ac23de14817922e5656e0f55e3e41b408288a
Usmon Kurbanbaev <usmonkurbanbaev1@gmail.com>
2023-01-30 15:03:35 +0500

    for loops final task

--- src/main/java/school/mjc/stage0/loops/finalTask/Power.java
+++ src/main/java/school/mjc/stage0/loops/finalTask/Power.java
@@ -2,7 +2,11 @@ package school.mjc.stage0.loops.finalTask;
 
 public class Power {
     public void printPower(int numberToPrint, int power) {
-
+        int smth = numberToPrint;
+        for(int i= 2; i <= power; i++){
+            numberToPrint*=smth;
+        }
+        System.out.println(numberToPrint);
     }
 
     public static void main(String[] args) {
--- src/main/java/school/mjc/stage0/loops/finalTask/Pyramid.java
+++ src/main/java/school/mjc/stage0/loops/finalTask/Pyramid.java
@@ -2,7 +2,21 @@ package school.mjc.stage0.loops.finalTask;
 
 public class Pyramid {
     public void printPyramid(int cathetusLength) {
-
+        for (int i = 1; i <= cathetusLength; i++){
+            for (int k = cathetusLength; k > 1; k--){
+                if(k <= i){
+                    System.out.print(k);
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            for (int j = 1; j <= cathetusLength; j++){
+                if(j <= i){
+                    System.out.print(j);
+                }
+            }
+            System.out.println();
+        }
     }
 
     public static void main(String[] args) {
